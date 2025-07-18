5cc534615add98006570e595c931466c4b1a3f0a
Mario Càllisto <mario.callisto@gmail.com>
2025-07-18 11:32:29 +0200

    TilingDCEL.maybeAddRegularPolygon wip

--- src/main/scala/dcel/TilingDataStructures.scala
+++ src/main/scala/dcel/TilingDataStructures.scala
@@ -2,6 +2,7 @@ package io.github.scala_tessella
 package dcel
 
 import scala.annotation.tailrec
+import scala.math.{atan2, cos, sin, toDegrees, toRadians}
 
 /**
  * Represents a vertex in the tiling.
@@ -9,7 +10,6 @@ import scala.annotation.tailrec
  * @param id A unique identifier for the vertex.
  * @param x  The x-coordinate of the vertex.
  * @param y  The y-coordinate of the vertex.
- * @param leaving An optional half-edge originating from this vertex.
  */
 case class Vertex(id: String, x: Double, y: Double):
   var leaving: Option[HalfEdge] = None
@@ -85,3 +85,96 @@ case class TilingDCEL(
 
         collectVertices(startEdge)
         builder.result()
+
+  /**
+   * Adds a new regular polygon to a specified boundary edge of the tiling.
+   *
+   * This method does not check for self-intersections.
+   *
+   * @param sides The number of sides of the regular polygon to add.
+   * @param onEdgeStartingWithVertexId The ID of the vertex where the boundary edge starts.
+   * @return Either an error message or a new TilingDCEL with the polygon added.
+   */
+  def maybeAddRegularPolygon(sides: Int, onEdgeStartingWithVertexId: String): Either[String, TilingDCEL] =
+    if sides < 3 then return Left(s"A polygon must have at least 3 sides, but $sides were specified.")
+
+    // Find the starting edge on the outer boundary
+    val boundaryEdge: Option[HalfEdge] =
+      @tailrec
+      def findEdge(current: HalfEdge, start: HalfEdge): Option[HalfEdge] =
+        if (current.origin.id == onEdgeStartingWithVertexId) Some(current)
+        else current.next match
+          case Some(next) if next ne start => findEdge(next, start)
+          case _ => None
+      outerFace.outerComponent.flatMap(start => findEdge(start, start))
+
+    boundaryEdge match
+      case None => Left(s"No boundary edge found starting with vertex ID '$onEdgeStartingWithVertexId'")
+      case Some(edgeToBuildOn) =>
+        val v1 = edgeToBuildOn.origin
+        val v2 = edgeToBuildOn.twin.get.origin
+
+        // 1. Calculate new vertex coordinates
+        val newVertexCount = this.vertices.size
+        val angle = (sides - 2) * 180.0 / sides
+        val turnAngle = 180.0 - angle
+        val newVertices = List.newBuilder[Vertex]
+        var currentPoint = v2
+        var heading = toDegrees(atan2(v2.y - v1.y, v2.x - v1.x))
+
+        for (i <- 0 until sides - 2)
+          heading += turnAngle
+          val newX = currentPoint.x + cos(toRadians(heading))
+          val newY = currentPoint.y + sin(toRadians(heading))
+          val newVertex = Vertex(s"V${newVertexCount + i}", newX, newY)
+          newVertices += newVertex
+          currentPoint = newVertex
+
+        val newVerticesList = newVertices.result()
+        val newPolygonVertices = List(v1, v2) ++ newVerticesList
+
+        // 2. Create new DCEL components
+        val newFace = Face(s"F${this.innerFaces.size + 1}")
+        val newEdgePairs = (1 until sides).map { i =>
+          val p1 = newPolygonVertices(i)
+          val p2 = newPolygonVertices((i + 1) % sides)
+          val inner = HalfEdge(p1)
+          val outer = HalfEdge(p2)
+          inner.twin = Some(outer)
+          outer.twin = Some(inner)
+          (inner, outer)
+        }
+        val newInnerEdges = newEdgePairs.map(_._1)
+        val newOuterEdges = newEdgePairs.map(_._2).reverse
+
+        // 3. Link the new polygon's inner cycle (CCW)
+        val allInnerEdges = edgeToBuildOn :: newInnerEdges.toList
+        for (i <- allInnerEdges.indices)
+          val current = allInnerEdges(i)
+          val next = allInnerEdges((i + 1) % sides)
+          current.next = Some(next); next.prev = Some(current)
+          current.incidentFace = Some(newFace)
+
+        newFace.outerComponent = Some(edgeToBuildOn)
+
+        // 4. Link the new outer boundary cycle (CW)
+        val oldPrevOuter = edgeToBuildOn.prev.get; val oldNextOuter = edgeToBuildOn.next.get
+        oldPrevOuter.next = Some(newOuterEdges.head); newOuterEdges.head.prev = Some(oldPrevOuter)
+        newOuterEdges.last.next = Some(oldNextOuter); oldNextOuter.prev = Some(newOuterEdges.last)
+
+        for (i <- newOuterEdges.indices)
+          newOuterEdges(i).incidentFace = Some(this.outerFace)
+          if i < newOuterEdges.length - 1 then
+            newOuterEdges(i).next = Some(newOuterEdges(i + 1))
+            newOuterEdges(i + 1).prev = Some(newOuterEdges(i))
+
+        if outerFace.outerComponent.contains(edgeToBuildOn) then
+          outerFace.outerComponent = Some(newOuterEdges.head)
+
+        // 5. Assemble and return the new TilingDCEL
+        Right(TilingDCEL(
+          vertices = this.vertices ++ newVerticesList,
+          halfEdges = this.halfEdges ++ newInnerEdges ++ newOuterEdges.reverse,
+          innerFaces = this.innerFaces :+ newFace,
+          outerFace = this.outerFace
+        ))
---
+++ src/test/scala/dcel/TilingAddPolygonSpec.scala
@@ -0,0 +1,46 @@
+package io.github.scala_tessella
+package dcel
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.EitherValues
+
+class TilingAddPolygonSpec extends AnyFlatSpec with Matchers with EitherValues:
+
+  behavior of "TilingDCEL.maybeAddRegularPolygon"
+
+  it should "fail to add a polygon with fewer than 3 sides" in {
+    val tiling = TilingBuilder.createRegularPolygon(4).value
+    val result = tiling.maybeAddRegularPolygon(2, "V1")
+    result.isLeft shouldBe true
+    result.left.value should include("must have at least 3 sides")
+  }
+
+  it should "fail if the specified boundary edge does not exist" in {
+    val tiling = TilingBuilder.createRegularPolygon(4).value
+    val result = tiling.maybeAddRegularPolygon(4, "V_NonExistent")
+    result.isLeft shouldBe true
+    result.left.value should include("No boundary edge found")
+  }
+
+//  it should "successfully add a square to the side of another square" in {
+//    // Start with a single square (V0-V1-V2-V3)
+//    val initialTiling = TilingBuilder.createRegularPolygon(4).value
+//    initialTiling.vertices.length shouldBe 4
+//    initialTiling.innerFaces.length shouldBe 1
+//    initialTiling.boundary.map(_.id) shouldBe Vector("V0", "V3", "V2", "V1")
+//
+//    // Add another square onto the edge starting at V1 (which is edge V1 -> V2)
+//    val result = initialTiling.maybeAddRegularPolygon(4, "V1")
+//    result.isRight shouldBe true
+//
+//    val newTiling = result.value
+//    // Should add 2 new vertices (V4, V5)
+//    newTiling.vertices.length shouldBe 6
+//    // Should add 1 new inner face
+//    newTiling.innerFaces.length shouldBe 2
+//
+//    // The new boundary should bypass V1 and V2
+//    val newBoundaryIds = newTiling.boundary.map(_.id)
+//    newBoundaryIds shouldBe Vector("V0", "V3", "V2", "V5", "V4", "V1")
+//  }
