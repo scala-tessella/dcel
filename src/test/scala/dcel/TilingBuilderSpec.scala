6d64e9a5acf6f69250341a304eddcff7f7fb55a0
Mario Càllisto <mario.callisto@gmail.com>
2025-07-24 10:33:53 +0200

    More testing

--- src/test/scala/dcel/TilingBuilderSpec.scala
+++ src/test/scala/dcel/TilingBuilderSpec.scala
@@ -135,6 +135,10 @@ class TilingBuilderSpec extends AnyFlatSpec with Matchers with EitherValues:
     tiling.faces.length shouldBe 2
     tiling.faces.map(_.id).mkString(", ") shouldBe "F_Outer, F_Poly"
     tiling.halfEdges.length shouldBe 8
+    tiling.outerFace.halfEdges.map(_.angle).mkString(", ") shouldBe "270, 270, 270, 270"
+    tiling.outerFace.halfEdges.map(_.incidentFace.get.id).mkString(", ") shouldBe "F_Outer, F_Outer, F_Outer, F_Outer"
+    tiling.innerFaces.map(_.halfEdges.map(_.angle).mkString(", ")) shouldBe List("90, 90, 90, 90")
+    tiling.innerFaces.map(_.halfEdges.map(_.incidentFace.get.id).mkString(", ")) shouldBe List("F_Poly, F_Poly, F_Poly, F_Poly")
   }
 
   it should "create a valid TilingDCEL for a regular pentagon" in {
@@ -144,6 +148,10 @@ class TilingBuilderSpec extends AnyFlatSpec with Matchers with EitherValues:
     tiling.vertices.length shouldBe 5
     tiling.faces.length shouldBe 2
     tiling.halfEdges.length shouldBe 10
+    tiling.outerFace.halfEdges.map(_.angle).mkString(", ") shouldBe "252, 252, 252, 252, 252"
+    tiling.outerFace.halfEdges.map(_.incidentFace.get.id).mkString(", ") shouldBe "F_Outer, F_Outer, F_Outer, F_Outer, F_Outer"
+    tiling.innerFaces.map(_.halfEdges.map(_.angle).mkString(", ")) shouldBe List("108, 108, 108, 108, 108")
+    tiling.innerFaces.map(_.halfEdges.map(_.incidentFace.get.id).mkString(", ")) shouldBe List("F_Poly, F_Poly, F_Poly, F_Poly, F_Poly")
   }
 
   it should "fail to create a polygon with fewer than 3 sides" in {
