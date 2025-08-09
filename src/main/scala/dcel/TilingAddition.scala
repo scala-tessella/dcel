a88a34ba74f2ad7aa0ae7f96481a33a991e2f4e2
mariocallisto <mario.callisto@gmail.com>
2025-08-09 12:35:20 +0200

    TilingAddition.addSimplePolygon wip

--- src/main/scala/dcel/TilingAddition.scala
+++ src/main/scala/dcel/TilingAddition.scala
@@ -491,7 +491,7 @@ object TilingAddition:
     sharedEdges.zipWithIndex.foreach((edge, index) => edge.angle = Some(polyAngles(index)))
 
     // Update last boundary edge angle
-    newBoundaryEdges.lastOption.foreach(_.angle = Some(boundaryAngles.newVertices.last))
+    newBoundaryEdges.lastOption.foreach(_.angle = Some(boundaryAngles.newVertices.last.conjugate))
 
     // Update existing boundary edge from end vertex
     originalBoundary.next.foreach { nextEdge =>
