0c11b4cbf7a068272ed9f4b746bc40ee0bcc0911
mariocallisto <mario.callisto@gmail.com>
2025-08-11 12:04:51 +0200

    Cleaner

--- src/test/scala/dcel/TilingAdditionSpec.scala
+++ src/test/scala/dcel/TilingAdditionSpec.scala
@@ -271,7 +271,7 @@ class TilingAdditionSpec extends AnyFlatSpec with Matchers with EitherValues:
 
     result.isRight shouldBe true
     val tiling = result.value
-    println(TilingDCEL.validate(tiling))
+//    println(TilingDCEL.validate(tiling))
 //    println(tiling.toSVG(leavingEdgeMarkers = true, faceIdsOnEdges = true))
     verifyValidTiling(tiling)
 
@@ -284,7 +284,7 @@ class TilingAdditionSpec extends AnyFlatSpec with Matchers with EitherValues:
 
     result.isRight shouldBe true
     val tiling = result.value
-    println(TilingDCEL.validate(tiling))
+//    println(TilingDCEL.validate(tiling))
 //    println(tiling.toSVG(leavingEdgeMarkers = true, faceIdsOnEdges = true))
     verifyValidTiling(tiling)
 
@@ -297,7 +297,7 @@ class TilingAdditionSpec extends AnyFlatSpec with Matchers with EitherValues:
 
     result.isRight shouldBe true
     val tiling = result.value
-    println(TilingDCEL.validate(tiling))
+//    println(TilingDCEL.validate(tiling))
 //    println(tiling.toSVG(leavingEdgeMarkers = true, faceIdsOnEdges = true))
     verifyValidTiling(tiling)
 
