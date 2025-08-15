e1d1e9314cef53b5e089558dfc303f8e20dc8ad3
mariocallisto <mario.callisto@gmail.com>
2025-08-15 17:04:29 +0200

    TilingSmallestSpec improved

--- src/test/scala/dcel/TilingSmallestSpec.scala
+++ src/test/scala/dcel/TilingSmallestSpec.scala
@@ -13,14 +13,52 @@ class TilingSmallestSpec extends AnyFlatSpec with Matchers with EitherValues:
 
   behavior of "TilingDCEL.addRegularPolygon"
 
-  it should "add the same irregular pentagon with a different orientation to a triangle, producing a valid DCEL" in {
+  it should "add a triangle to a triangle, producing the smallest DCEL with a non boundary edge" in {
+    val triangle = TilingBuilder.createRegularPolygon(3).value
+    val result = triangle.addRegularPolygon(3, "V1")
+
+    result.isRight shouldBe true
+    val tiling = result.value
+    println(tiling.toSVG(leavingEdgeMarkers = true, faceIdsOnEdges = true))
+
+    // Check structure
+    tiling.vertices should have size 4
+    tiling.innerFaces should have size 2
+    tiling.halfEdges should have size 10 // 4 boundary + 6 inner edges
+
+    // Check boundary angles
+    tiling.outerFace.halfEdgesSafe.map(_.angle.get.toString).mkString(", ") shouldBe "240, 300, 240, 300"
+    tiling.outerFace.halfEdgesSafe.map(_.incidentFace.get.id).mkString(", ") shouldBe "F0, F0, F0, F0"
+
+    // Check inner face angles
+    tiling.innerFaces.map(_.halfEdgesSafe.map(_.angle.get.toString).mkString(", ")) shouldBe List("60, 60, 60", "60, 60, 60")
+    tiling.innerFaces.map(_.halfEdgesSafe.map(_.incidentFace.get.id).mkString(", ")) shouldBe List("F1, F1, F1", "F2, F2, F2")
+
+    // Check boundary
+    val boundary = tiling.boundary
+    boundary should have length 4
+    boundary.map(_.id) should contain theSameElementsInOrderAs Vector("V1", "V4", "V3", "V2")
+  }
+
+  it should "add three triangles to a triangle, producing the smallest DCEL with a non boundary face" in {
+    val triangle = TilingBuilder.createRegularPolygon(3).value
+    val result = triangle
+      .addRegularPolygon(3, "V1").value
+      .addRegularPolygon(3, "V2").value
+      .addRegularPolygon(3, "V3")
+
+    result.isRight shouldBe true
+    val tiling = result.value
+    println(tiling.toSVG(leavingEdgeMarkers = true, faceIdsOnEdges = true))
+  }
+
+  it should "add a triangle to an irregular pentagon, producing the smallest DCEL with a non boundary vertex" in {
     val triangle = TilingBuilder.createSimplePolygon(
       List(AngleDegree(90), AngleDegree(90), AngleDegree(30), AngleDegree(300), AngleDegree(30))
     ).value
     val result = triangle.addRegularPolygon(3, "V4")
 
     result.isRight shouldBe true
-    val tiling = result.value
-    println(TilingDCEL.validate(tiling))
-    println(tiling.toSVG(leavingEdgeMarkers = true, faceIdsOnEdges = true))
+//    val tiling = result.value
+//    println(tiling.toSVG(leavingEdgeMarkers = true, faceIdsOnEdges = true))
   }
