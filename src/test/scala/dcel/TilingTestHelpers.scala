e9d449ca0bb67c9079f91dfb5929c545d896d5fa
mariocallisto <mario.callisto@gmail.com>
2025-08-23 20:22:36 +0200

    TilingTestHelpers added

---
+++ src/test/scala/dcel/TilingTestHelpers.scala
@@ -0,0 +1,22 @@
+package io.github.scala_tessella.dcel
+
+import org.scalatest.EitherValues
+
+/** A trait for test classes with helper methods to create tiling fixtures. */
+trait TilingTestHelpers extends EitherValues:
+
+  /** A tiling with a single triangle
+   * <img src="file:../../resources/triangle.svg"/>
+   **/
+  def triangle: TilingDCEL =
+    TilingBuilder.createRegularPolygon(3).value
+
+  /** A tiling with a single square
+   * <img src="file:../../resources/square.svg"/>
+   **/
+  def square: TilingDCEL =
+    TilingBuilder.createRegularPolygon(4).value
+
+  /** A tiling with a single hexagon */
+  def hexagon: TilingDCEL =
+    TilingBuilder.createRegularPolygon(6).value
