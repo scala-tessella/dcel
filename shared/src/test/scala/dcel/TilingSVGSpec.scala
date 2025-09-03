e6fd7e3ff99d7caf149aa2d504c12009f3b06e12
Mario Càllisto <mario.callisto@gmail.com>
2025-09-03 18:57:54 +0200

    Cleaner

--- shared/src/test/scala/dcel/TilingSVGSpec.scala
+++ shared/src/test/scala/dcel/TilingSVGSpec.scala
@@ -2,7 +2,6 @@ package dcel
 
 import dcel.BigDecimalGeometry.BigPoint
 import dcel.TilingSVG.*
-import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
