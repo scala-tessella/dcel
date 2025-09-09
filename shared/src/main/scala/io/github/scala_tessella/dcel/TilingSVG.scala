0aa8943f834ab08dd5dea5b0f8e6f67b22d32fdd
Mario Càllisto <mario.callisto@gmail.com>
2025-09-09 23:07:02 +0200

    TilingSVG.toMetadata wip

--- shared/src/main/scala/io/github/scala_tessella/dcel/TilingSVG.scala
+++ shared/src/main/scala/io/github/scala_tessella/dcel/TilingSVG.scala
@@ -1,10 +1,11 @@
 package io.github.scala_tessella.dcel
 
-import BigDecimalGeometry._
-import spire.implicits._
+import BigDecimalGeometry.*
+import spire.implicits.*
+import spire.math.Rational
 
 import scala.collection.mutable
-import scala.xml._
+import scala.xml.*
 
 object TilingSVG:
 
@@ -470,3 +471,173 @@ object TilingSVG:
         leavingEdgeMarkers = config.leavingEdgeMarkers,
         faceIdsOnEdges = config.faceIdsOnEdges
       )
+
+    /** Serializes the complete structure of a [[TilingDCEL]] into XML metadata, which can be embedded within an SVG. This
+      * metadata includes all vertices, half-edges, and faces, along with their properties and relationships,
+      * ensuring that the [[TilingDCEL]] can be fully reconstructed later.
+      */
+    def toMetadata: String =
+
+      val halfEdgeIds: Map[HalfEdge, Int] = tiling.halfEdges.zipWithIndex.toMap
+
+      val vertexNodes  = tiling.vertices.map { v =>
+        val attrsList = List(
+          Some("id" -> v.id.value),
+          Some("x"  -> v.coords.x.toString),
+          Some("y"  -> v.coords.y.toString),
+          v.leaving.flatMap(halfEdgeIds.get).map(id => "leaving" -> id)
+        ).flatten
+        elem("vertex", attrs(attrsList*))
+      }
+      val verticesElem = elem("vertices", children = vertexNodes)
+
+      val halfEdgeNodes = tiling.halfEdges.zipWithIndex.map { case (he, id) =>
+        val attrsList = List(
+          Some("id"     -> id),
+          Some("origin" -> he.origin.id.value),
+          he.twin.flatMap(halfEdgeIds.get).map(twinId => "twin" -> twinId),
+          he.next.flatMap(halfEdgeIds.get).map(nextId => "next" -> nextId),
+          he.prev.flatMap(halfEdgeIds.get).map(prevId => "prev" -> prevId),
+          he.incidentFace.map(f => "face" -> f.id.value),
+          he.angle.map(a => "angle" -> a.toRational)
+        ).flatten
+        elem("half-edge", attrs(attrsList*))
+      }
+      val halfEdgesElem = elem("half-edges", children = halfEdgeNodes)
+
+      val faceNodes = tiling.faces.map { f =>
+        val attrsList = List(
+          Some("id" -> f.id.value),
+          f.outerComponent.flatMap(halfEdgeIds.get).map(id => "outer-component" -> id)
+        ).flatten
+        val innerIds  = f.innerComponents.flatMap(_.flatMap(halfEdgeIds.get))
+        val allAttrs  = if innerIds.nonEmpty then
+          attrsList :+ ("inner-components" -> innerIds.mkString(","))
+        else
+          attrsList
+        elem("face", attrs(allAttrs*))
+      }
+      val facesElem = elem("faces", children = faceNodes)
+
+      val dcelElem = elem(
+        "tessella-dcel",
+        children = Seq(verticesElem, halfEdgesElem, facesElem),
+        prefix = Some("tessella"),
+        scope = NamespaceBinding("tessella", "https://github.com/scala-tessella/tessella", TopScope)
+      )
+
+      dcelElem.toString
+
+  /** Deserializes the XML metadata that includes all vertices, half-edges, and faces,
+   *  along with their properties and relationships,
+   *  and fully reconstructs the complete structure of a [[TilingDCEL]].
+   */
+  def fromMetadata(metadata: String): Either[TilingError, TilingDCEL] =
+
+    import scala.util.Try
+    import scala.xml.{Node, XML}
+
+    implicit class SequenceOps[E, A](eithers: List[Either[E, A]]) {
+      def sequence: Either[E, List[A]] =
+        eithers.foldRight(Right(Nil): Either[E, List[A]]) { (e, acc) =>
+          for (es <- acc; x <- e) yield x :: es
+        }
+    }
+
+    implicit class OptionTraverse[A](opt: Option[A]) {
+      def traverse[E, B](f: A => Either[E, B]): Either[E, Option[B]] =
+        opt match {
+          case Some(a) => f(a).map(Some(_))
+          case None => Right(None)
+        }
+    }
+
+    def getAttr(node: Node, attr: String): Either[ValidationError, String] =
+      node.attribute(attr).map(_.text).toRight(ValidationError(s"${node.label} missing '$attr'"))
+
+    def attrAs[T](node: Node, attr: String, f: String => T, typeName: String): Either[ValidationError, T] =
+      for {
+        str <- getAttr(node, attr)
+        res <- Try(f(str)).toEither.left.map(e => ValidationError(s"Invalid $typeName in ${node.label} attribute '$attr': ${e.getMessage}"))
+      } yield res
+
+    for {
+      xmlRoot <- Try(XML.loadString(metadata)).toEither.left.map(e => ValidationError(s"Invalid XML: ${e.getMessage}"))
+
+      vertexNodes = (xmlRoot \ "vertices" \ "vertex").toList
+      vertices <- vertexNodes.map { vNode =>
+        for {
+          id <- getAttr(vNode, "id")
+          x <- attrAs(vNode, "x", BigDecimal.apply, "BigDecimal")
+          y <- attrAs(vNode, "y", BigDecimal.apply, "BigDecimal")
+        } yield Vertex(VertexId(id), BigPoint(x, y))
+      }.sequence
+      vertexMap = vertices.map(v => v.id -> v).toMap
+
+      halfEdgeNodes = (xmlRoot \ "half-edges" \ "half-edge").toList
+      halfEdges <- halfEdgeNodes.map { heNode =>
+        for {
+          originId <- getAttr(heNode, "origin")
+          origin <- vertexMap.get(VertexId(originId)).toRight(NotFoundError("Vertex for half-edge origin", originId))
+        } yield HalfEdge(origin)
+      }.sequence
+      halfEdgeMap = halfEdges.zipWithIndex.map { case (he, i) => i -> he }.toMap
+
+      faceNodes = (xmlRoot \ "faces" \ "face").toList
+      faces <- faceNodes.map { fNode =>
+        getAttr(fNode, "id").map(id => Face(FaceId(id)))
+      }.sequence
+      faceMap = faces.map(f => f.id -> f).toMap
+
+      _ <- vertexNodes.zip(vertices).map { case (vNode, vertex) =>
+        vNode.attribute("leaving").map(_.text).traverse { leavingIdStr =>
+          for {
+            leavingId <- Try(leavingIdStr.toInt).toEither.left.map(_ => ValidationError(s"Invalid leaving ID: $leavingIdStr"))
+            leavingEdge <- halfEdgeMap.get(leavingId).toRight(NotFoundError("Leaving edge", leavingId.toString))
+          } yield vertex.leaving = Some(leavingEdge)
+        }
+      }.sequence
+
+      _ <- halfEdgeNodes.zip(halfEdges).map { case (heNode, he) =>
+        for {
+          twinId <- attrAs(heNode, "twin", _.toInt, "Int")
+          twinEdge <- halfEdgeMap.get(twinId).toRight(NotFoundError("Twin edge", twinId.toString))
+          _ = he.twin = Some(twinEdge)
+          nextId <- attrAs(heNode, "next", _.toInt, "Int")
+          nextEdge <- halfEdgeMap.get(nextId).toRight(NotFoundError("Next edge", nextId.toString))
+          _ = he.next = Some(nextEdge)
+          prevId <- attrAs(heNode, "prev", _.toInt, "Int")
+          prevEdge <- halfEdgeMap.get(prevId).toRight(NotFoundError("Prev edge", prevId.toString))
+          _ = he.prev = Some(prevEdge)
+          faceId <- getAttr(heNode, "face")
+          incidentFace <- faceMap.get(FaceId(faceId)).toRight(NotFoundError("Incident face", faceId))
+          _ = he.incidentFace = Some(incidentFace)
+          angleStr <- getAttr(heNode, "angle")
+          angle = AngleDegree(Rational(angleStr))//.toRight(ValidationError(s"Invalid angle format: $angleStr"))
+          _ = he.angle = Some(angle)
+        } yield ()
+      }.sequence
+
+      _ <- faceNodes.zip(faces).map { case (fNode, f) =>
+        val z = fNode.attribute("outer-component").map(_.text).traverse { ocIdStr =>
+          for
+            ocId <- Try(ocIdStr.toInt).toEither.left.map(_ => ValidationError(s"Invalid outer-component ID: $ocIdStr"))
+            ocEdge <- halfEdgeMap.get(ocId).toRight(NotFoundError("Outer component edge", ocId.toString))
+          yield f.outerComponent = Some(ocEdge)
+        }
+        fNode.attribute("inner-components").map(_.text).filter(_.nonEmpty).traverse { icIdsStr =>
+          val idsEither = icIdsStr.split(',').toList.map(idStr =>
+            Try(idStr.trim.toInt).toEither.left.map(_ => ValidationError(s"Invalid inner-component ID: $idStr"))
+          ).sequence
+          for {
+            ids <- idsEither
+            icEdges <- ids.map(id => halfEdgeMap.get(id).toRight(NotFoundError("Inner component edge", id.toString))).sequence
+          } yield f.innerComponents = icEdges.map(Some(_))
+        }
+      }.sequence
+
+      outerFace <- faceMap.get(FaceId.outerId).toRight(ValidationError("Outer face (ID 0) not found in metadata"))
+      innerFaces = faces.filterNot(_.id == FaceId.outerId)
+      tiling = TilingDCEL.fromUntrusted(vertices, halfEdges, innerFaces, outerFace)
+      validated <- tiling
+    } yield validated
--- shared/src/test/scala/io/github/scala_tessella/dcel/TilingSVGSpec.scala
+++ shared/src/test/scala/io/github/scala_tessella/dcel/TilingSVGSpec.scala
@@ -1,7 +1,9 @@
 package io.github.scala_tessella.dcel
 
 import BigDecimalGeometry.BigPoint
-import TilingSVG._
+import TilingSVG.*
+import TilingEquivalency.isEquivalentTo
+
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -668,4 +670,114 @@ class TilingSVGSpec extends AnyFlatSpec with Matchers with TilingTestHelpers:
         )
       }
     )
+
   }
+
+  behavior of "TilingSVG.toMetadata"
+
+  it should "generate metadata for an empty tiling" in {
+    val metadataXml = scala.xml.XML.loadString(emptyTiling.toMetadata)
+    val vertices    = metadataXml \ "vertices" \ "vertex"
+    val halfEdges   = metadataXml \ "half-edges" \ "half-edge"
+    val faces       = metadataXml \ "faces" \ "face"
+
+    allAssert(
+      metadataXml.scope.getURI("tessella") shouldBe "https://github.com/scala-tessella/tessella",
+      metadataXml.label shouldBe "tessella-dcel",
+      metadataXml.prefix shouldBe "tessella",
+      vertices.isEmpty shouldBe true,
+      halfEdges.isEmpty shouldBe true,
+      faces.size shouldBe 1,
+      (faces.head \@ "id") shouldBe "F0",
+      faces.head.attribute("outer-component").isEmpty shouldBe true
+    )
+  }
+
+  it should "generate correct metadata for a single triangle tiling" in {
+    val triangle    = createTriangleTiling()
+    val metadataXml = scala.xml.XML.loadString(triangle.toMetadata)
+
+    allAssert(
+      metadataXml.scope.getURI("tessella") shouldBe "https://github.com/scala-tessella/tessella",
+      metadataXml.label shouldBe "tessella-dcel",
+      metadataXml.prefix shouldBe "tessella"
+    )
+
+    val vertices = metadataXml \ "vertices" \ "vertex"
+    vertices.size shouldBe 3
+    vertices.foreach { v =>
+
+      allAssert(
+        v.attribute("id") shouldBe defined,
+        v.attribute("x") shouldBe defined,
+        v.attribute("y") shouldBe defined,
+        v.attribute("leaving") shouldBe defined
+      )
+    }
+
+    val halfEdges = metadataXml \ "half-edges" \ "half-edge"
+    halfEdges.size shouldBe 6
+    halfEdges.foreach { he =>
+
+      allAssert(
+        he.attribute("id") shouldBe defined,
+        he.attribute("origin") shouldBe defined,
+        he.attribute("twin") shouldBe defined,
+        he.attribute("next") shouldBe defined,
+        he.attribute("prev") shouldBe defined,
+        he.attribute("face") shouldBe defined,
+        he.attribute("angle") shouldBe defined
+      )
+    }
+
+    val faces = metadataXml \ "faces" \ "face"
+    faces.size shouldBe 2
+    faces.foreach { f =>
+
+      f.attribute("id") shouldBe defined
+    }
+    // All faces in a complete tiling must have an outer component
+    faces.count(_.attribute("outer-component").isDefined) shouldBe 2
+  }
+
+  it should "generate correct metadata for a single square tiling" in {
+    val square      = createSquareTiling()
+    val metadataXml = scala.xml.XML.loadString(square.toMetadata)
+
+    allAssert(
+      metadataXml.scope.getURI("tessella") shouldBe "https://github.com/scala-tessella/tessella",
+      metadataXml.label shouldBe "tessella-dcel",
+      metadataXml.prefix shouldBe "tessella"
+    )
+
+    val vertices = metadataXml \ "vertices" \ "vertex"
+    vertices.size shouldBe 4
+
+    val halfEdges = metadataXml \ "half-edges" \ "half-edge"
+    halfEdges.size shouldBe 8
+
+    val faces = metadataXml \ "faces" \ "face"
+    faces.size shouldBe 2
+  }
+
+  behavior of "TilingSVG.fromMetadata"
+
+  it should "successfully reconstruct an empty tiling from metadata" in {
+    val metadata = emptyTiling.toMetadata
+    val reconstructed = TilingSVG.fromMetadata(metadata)
+    println(reconstructed)
+    reconstructed.isRight shouldBe true
+    reconstructed.value.isEmpty shouldBe true
+  }
+
+  it should "successfully reconstruct a single triangle from metadata (round-trip)" in {
+    val triangle = createTriangleTiling()
+    val metadata = triangle.toMetadata
+    val reconstructed = TilingSVG.fromMetadata(metadata)
+    allAssert(
+      reconstructed.isRight shouldBe true,
+      TilingDCEL.validate(reconstructed.value) shouldBe Right(()),
+      triangle.isEquivalentTo(reconstructed.value) shouldBe true
+    )
+  }
+
