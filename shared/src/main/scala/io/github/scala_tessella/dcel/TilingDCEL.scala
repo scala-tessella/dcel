b93761132418a9d5016fe23a56f43bc97b7e0344
mariocallisto <mario.callisto@gmail.com>
2025-10-24 21:49:14 +0200

    Refactored

--- shared/src/main/scala/io/github/scala_tessella/dcel/TilingDCEL.scala
+++ shared/src/main/scala/io/github/scala_tessella/dcel/TilingDCEL.scala
@@ -387,6 +387,22 @@ final case class TilingDCEL private (
   def uniformity: Map[List[Int], List[VertexId]] =
     val boundaryVertexIds = boundaryVertices.map(_.id)
 
+    def vertexIdClasses(dcels: List[(VertexId, TilingDCEL)]): List[List[VertexId]] =
+      val classes = mutable.ArrayBuffer[(TilingDCEL, List[VertexId])]()
+      dcels.foreach { case (vertexId, local) =>
+        // Try to find an existing equivalent representative
+        classes.indexWhere { case (rep, _) =>
+          local.isEquivalentTo(rep)
+        } match
+          case -1  =>
+            classes += ((local, List(vertexId)))
+          case idx =>
+            val (rep, ids) = classes(idx)
+            classes.update(idx, (rep, vertexId :: ids))
+      }
+
+      classes.toList.map(_._2.reverse)
+
     def loop(
         key: List[Int],
         vertexIds: List[VertexId],
@@ -395,25 +411,11 @@ final case class TilingDCEL private (
       if vertexIds.isEmpty then
         acc.reverse
       else
-        val distance = key.length
-        val dcels    = vertexIds.map(vertexId => vertexId -> getDcelAtVertex(vertexId, distance).toOption.get)
-        // Group by equivalence class using a canonical representative per class
-        val classes  = mutable.ArrayBuffer[(TilingDCEL, List[VertexId])]()
-        dcels.foreach { case (vertexId, local) =>
-          // Try to find an existing equivalent representative
-          classes.indexWhere { case (rep, _) =>
-            local.isEquivalentTo(rep)
-          } match
-            case -1  =>
-              classes += ((local, List(vertexId)))
-            case idx =>
-              val (rep, ids) = classes(idx)
-              classes.update(idx, (rep, vertexId :: ids))
-        }
-
+        val distance                                            = key.length
+        val dcels                                               = vertexIds.map(vertexId => vertexId -> getDcelAtVertex(vertexId, distance).toOption.get)
+        val classes: List[List[VertexId]]                       = vertexIdClasses(dcels)
         val dcelMaps                                            = dcels.toMap
-        val vertexIdClasses: List[List[VertexId]]               = classes.toList.map(_._2.reverse)
-        val partitioned: List[(List[VertexId], List[VertexId])] = vertexIdClasses.map(_.partition(vertexId =>
+        val partitioned: List[(List[VertexId], List[VertexId])] = classes.map(_.partition(vertexId =>
           val localBoundaryVertexIds = dcelMaps(vertexId).boundaryVertices.map(_.id)
           boundaryVertexIds.intersect(localBoundaryVertexIds).isEmpty
         ))
