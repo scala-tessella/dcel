87adaa8f430e4b46a970d94ec9435cac1efaaf7d
Mario Càllisto <mario.callisto@gmail.com>
2025-10-29 20:29:14 +0100

    Uniform issue solved

--- shared/src/test/scala/io/github/scala_tessella/dcel/TilingUniformitySpec.scala
+++ shared/src/test/scala/io/github/scala_tessella/dcel/TilingUniformitySpec.scala
@@ -1,7 +1,7 @@
 package io.github.scala_tessella.dcel
 
-import io.github.scala_tessella.dcel.TilingValidation.validate
-import io.github.scala_tessella.dcel.geometry.{AngleDegree, RegularPolygon, SimplePolygon}
+import io.github.scala_tessella.dcel.geometry.RegularPolygon
+import io.github.scala_tessella.dcel.structure.VertexId
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -23,7 +23,6 @@ class TilingUniformitySpec extends AnyFlatSpec with Matchers with TilingTestHelp
 
     /** <img src="file:../../../../../resources/uniform1_specular.svg"/> */
     val result = TilingBuilder.createHoledTriangleNet(9, 9)((i, j) => (j + 3 * i) % 7 == 0)
-    println(result.toSVG(showUniformity = true))
     allAssert(
       result.uniformityTree.sizeLeaves shouldBe 1,
       result.innerFaces.size shouldBe 101
@@ -77,5 +76,27 @@ class TilingUniformitySpec extends AnyFlatSpec with Matchers with TilingTestHelp
     allAssert(
       result.uniformityTree.sizeLeaves shouldBe 6,
       result.innerFaces.size shouldBe 131
+    )
+  }
+
+  it should "find an issue" in {
+    /** <img src="file:../../../../../resources/uniform_issue.svg"/> */
+    val result =
+      TilingBuilder.createHoledTriangleNet(9, 11)((i, j) => (i - j) % 3 == 0)
+        .maybeAddRegularPolygon(VertexId("V24"), VertexId("V25"), RegularPolygon(3)).value
+        .maybeAddRegularPolygon(VertexId("V25"), VertexId("V35"), RegularPolygon(3)).value
+        .maybeAddRegularPolygon(VertexId("V27"), VertexId("V28"), RegularPolygon(3)).value
+        .maybeAddRegularPolygon(VertexId("V28"), VertexId("V38"), RegularPolygon(3)).value
+        .maybeAddRegularPolygon(VertexId("V54"), VertexId("V55"), RegularPolygon(3)).value
+        .maybeAddRegularPolygon(VertexId("V55"), VertexId("V65"), RegularPolygon(3)).value
+        .maybeAddRegularPolygon(VertexId("V57"), VertexId("V58"), RegularPolygon(3)).value
+        .maybeAddRegularPolygon(VertexId("V58"), VertexId("V68"), RegularPolygon(3)).value
+        .maybeAddRegularPolygon(VertexId("V84"), VertexId("V85"), RegularPolygon(3)).value
+        .maybeAddRegularPolygon(VertexId("V85"), VertexId("V95"), RegularPolygon(3)).value
+        .maybeAddRegularPolygon(VertexId("V87"), VertexId("V88"), RegularPolygon(3)).value
+        .maybeAddRegularPolygon(VertexId("V88"), VertexId("V98"), RegularPolygon(3)).value
+    allAssert(
+      result.uniformityTree.sizeLeaves shouldBe 5,
+      result.innerFaces.size shouldBe 39
     )
   }
