fe744257ac24eb8c149a5d13be99a7c578ae3e2f
Mario Càllisto <mario.callisto@gmail.com>
2025-11-20 23:16:32 +0100

    More testing

--- shared/src/test/scala/io/github/scala_tessella/dcel/TilingDoubleSpec.scala
+++ shared/src/test/scala/io/github/scala_tessella/dcel/TilingDoubleSpec.scala
@@ -62,6 +62,25 @@ class TilingDoubleSpec extends AnyFlatSpec with Matchers with TilingTestHelpers:
 
   behavior of "TilingDCEL.quadrupleArea"
 
+  it should "keep an empty tiling" in {
+    TilingDCEL.empty.quadrupleArea.value.isEmpty shouldBe true
+  }
+
+  it should "fail for an hexagon" in {
+    TilingBuilder.createRegularPolygon(RegularPolygon(6)).quadrupleArea.isLeft shouldBe true
+  }
+
+  it should "quadruple a square" in {
+    val doubled = TilingBuilder.createRegularPolygon(RegularPolygon(4)).quadrupleArea
+    val result = doubled.value
+    allAssert(
+      result.halfEdges.size shouldBe 24,
+      result.vertices.size shouldBe 9,
+      result.innerFaces.size shouldBe 4,
+      TilingValidation.validate(result).isRight shouldBe true
+    )
+  }
+
   it should "quadruple a 2x2 square net" in {
     val doubled = TilingBuilder.createRhombusNet(2, 2).quadrupleArea
     val result  = doubled.value
