88ae20cd6ee513f46e77e23f369fe0d0af297616
mariocallisto <mario.callisto@gmail.com>
2025-11-21 17:03:06 +0100

    Moved tests to TilingDCELSpec

--- shared/src/test/scala/io/github/scala_tessella/dcel/TilingDCELSpec.scala
+++ shared/src/test/scala/io/github/scala_tessella/dcel/TilingDCELSpec.scala
@@ -470,3 +470,47 @@ class TilingDCELSpec extends AnyFlatSpec with Matchers with TilingTestHelpers:
         )
       )
   }
+
+  behavior of "TilingDCEL.quadrupleArea"
+
+  it should "keep an empty tiling" in {
+    TilingDCEL.empty.quadrupleArea.value.isEmpty shouldBe true
+  }
+
+  it should "fail for an hexagon" in {
+    hexagon.quadrupleArea.isRight shouldBe false
+  }
+
+  it should "quadruple a square" in {
+    val doubled = square.quadrupleArea
+    val result = doubled.value
+    allAssert(
+      result.halfEdges.size shouldBe 24,
+      result.vertices.size shouldBe 9,
+      result.innerFaces.size shouldBe 4,
+      TilingValidation.validate(result).isRight shouldBe true
+    )
+  }
+
+  it should "quadruple a 2x2 square net" in {
+    val doubled = TilingBuilder.createRhombusNet(2, 2).quadrupleArea
+    val result = doubled.value
+    allAssert(
+      result.halfEdges.size shouldBe 80,
+      result.vertices.size shouldBe 25,
+      result.innerFaces.size shouldBe 16,
+      TilingValidation.validate(result).isRight shouldBe true
+    )
+  }
+
+  it should "quadruple a 2x2 hexagon net" in {
+    val doubled = TilingBuilder.createHexagonNet(2, 2).quadrupleArea
+    val result = doubled.value
+    allAssert(
+      result.halfEdges.size shouldBe 126,
+      result.vertices.size shouldBe 48,
+      result.innerFaces.size shouldBe 16,
+      TilingValidation.validate(result).isRight shouldBe true
+    )
+  }
+
