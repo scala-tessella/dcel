574d8a2b5c09669a298b81dc694fcc4397620668
Mario Càllisto <mario.callisto@gmail.com>
2025-11-24 11:07:20 +0100

    SimplePolygon.parallelogonIndices issue added

--- shared/src/test/scala/io/github/scala_tessella/dcel/geometry/SimplePolygonSpec.scala
+++ shared/src/test/scala/io/github/scala_tessella/dcel/geometry/SimplePolygonSpec.scala
@@ -86,3 +86,13 @@ class SimplePolygonSpec extends AnyFlatSpec with Matchers with TilingTestHelpers
     )
   }
 
+  behavior of "SimplePolygon.parallelogonIndices"
+
+  it should "be found for a 3.3.3.3.6 tessellation unit" in {
+
+    /** <img src="file:../../../../../../resources/simple/unit-3.3.3.3.6.svg"/> */
+    val unit = SimplePolygon(120, 180, 120, 120, 240, 180, 120, 240, 60, 180, 120, 180, 120, 240, 180, 120)
+    unit.parallelogonIndices.isDefined shouldBe true
+  }
+
+
