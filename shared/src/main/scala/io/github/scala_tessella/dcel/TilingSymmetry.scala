ebce04cbc061a4cfd6611efb1c501d5c375223ad
Mario Càllisto <mario.callisto@gmail.com>
2025-12-04 11:08:19 +0100

    Cleaner

--- shared/src/main/scala/io/github/scala_tessella/dcel/TilingSymmetry.scala
+++ shared/src/main/scala/io/github/scala_tessella/dcel/TilingSymmetry.scala
@@ -1,40 +1,12 @@
 package io.github.scala_tessella.dcel
 
-import io.github.scala_tessella.dcel.TilingEquivalency.groupByBoundaryEquivalency
 import io.github.scala_tessella.dcel.structure.{HalfEdge, VertexId}
-import io.github.scala_tessella.ring_seq.RingSeq.*
 
 import scala.collection.mutable
 
 object TilingSymmetry:
 
   extension (tiling: TilingDCEL)
-
-    /** Boundary angles rotational symmetry */
-    def boundaryRotationalSymmetry: Int =
-      tiling.boundaryEdges.map(_.angle.get).rotationalSymmetry
-
-    /** Boundary angles reflectional symmetry */
-    def boundaryReflectionalSymmetry: Int =
-      tiling.boundaryEdges.map(_.angle.get).symmetry
-
-    /** Boundary vertices mapped to DCELs equivalency classes */
-    private def boundaryVertexClasses: Vector[Int] =
-      val boundaryVertexIds        = tiling.boundaryVertices.map(_.id)
-      val centeredTilings          =
-        boundaryVertexIds.toList.map(vertexId => (vertexId, tiling.getDcelAtVertex(vertexId).toOption.get))
-      val vertexIdClassAssociation = groupByBoundaryEquivalency(centeredTilings).zipWithIndex.flatMap {
-        case (classes, index) => classes.map(classId => (classId, index))
-      }.toMap
-      boundaryVertexIds.map(vertexIdClassAssociation)
-
-    /** Boundary DCELs rotational symmetry */
-    def boundaryVerticesRotationalSymmetry: Int =
-      boundaryVertexClasses.rotationalSymmetry
-
-    /** Boundary DCELs reflectional symmetry */
-    def boundaryVerticesReflectionalSymmetry: Int =
-      boundaryVertexClasses.symmetry
 
     /** Checks if the tiling structure starting at edge `a` is isomorphic to the structure starting at edge
       * `b`.
@@ -114,3 +86,6 @@ object TilingSymmetry:
       val segmentSize       = boundaryAngles.size / symmetryOrder
       val first             = (0 until segmentSize).maxBy(boundaryAngles(_).toRational)
       (0 until symmetryOrder).toList.map(first + _ * segmentSize).map(boundaryVertexIds)
+
+    def reflectionalSymm: Int =
+      ???
