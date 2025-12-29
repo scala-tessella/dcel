f59e53b4a75c294ed4d21086f580caac49662cfa
mariocallisto <mario.callisto@gmail.com>
2025-12-29 15:50:00 +0100

    Better doc

--- shared/src/main/scala/io/github/scala_tessella/dcel/Utils.scala
+++ shared/src/main/scala/io/github/scala_tessella/dcel/Utils.scala
@@ -3,6 +3,15 @@ package io.github.scala_tessella.dcel
 object Utils:
 
   extension [E, A](eithers: List[Either[E, A]])
+
+    /** Transforms a list of `Either` values into a single `Either` containing a list of successful values or
+      * the first encountered error.
+      *
+      * @return
+      *   an `Either` containing:
+      *   - `Right(List[A])` if all elements in the list are `Right`
+      *   - `Left(E)` if at least one element in the list is `Left`, with the first error encountered.
+      */
     def sequence: Either[E, List[A]] =
       eithers.foldRight(Right(Nil): Either[E, List[A]]): (e, acc) =>
         for
@@ -10,9 +19,17 @@ object Utils:
           x  <- e
         yield x :: xs
 
-  extension [A](opt: Option[A])
+  extension [A](maybeA: Option[A])
+
+    /** Applies a function to the value inside an Option, propagating errors using Either.
+      *
+      * @param f
+      *   the function to be applied to the value inside the Option, producing an Either result.
+      * @return
+      *   either an error of type E or an Option containing the transformed value of type B.
+      */
     def traverse[E, B](f: A => Either[E, B]): Either[E, Option[B]] =
-      opt match
+      maybeA match
         case Some(a) =>
           f(a).map: b =>
             Some(b)
@@ -35,6 +52,14 @@ object Utils:
           elem -> f(elem)
         .toMap
 
+    /** Creates a `Map` where each key is the result of applying a function to the elements of the sequence
+      * and the corresponding value is the original element.
+      *
+      * @param f
+      *   the function used to transform each element to a key in the resulting `Map`.
+      * @return
+      *   a `Map` where keys are the transformed elements and values are the original elements.
+      */
     def associateValues[T](f: A => T): Map[T, A] =
       seq.iterator
         .map: elem =>
