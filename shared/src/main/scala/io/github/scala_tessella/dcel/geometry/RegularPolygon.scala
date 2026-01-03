ee5ad4e9c326bcfe64a71a760411a3c3a5dd22aa
mariocallisto <mario.callisto@gmail.com>
2026-01-03 15:30:00 +0100

    RegularPolygon.fromInteriorAngle added

--- shared/src/main/scala/io/github/scala_tessella/dcel/geometry/RegularPolygon.scala
+++ shared/src/main/scala/io/github/scala_tessella/dcel/geometry/RegularPolygon.scala
@@ -17,6 +17,20 @@ object RegularPolygon:
     if sides > 2 then sides
     else throw new IllegalArgumentException(s"Invalid number of sides: $sides")
 
+  /** Creates a RegularPolygon from a given interior angle.
+    *
+    * @param alpha
+    *   The interior angle of the polygon in degrees.
+    * @return
+    *   A RegularPolygon with a corresponding number of sides.
+    * @throws IllegalArgumentException
+    *   If the provided interior angle does not form a valid regular polygon.
+    */
+  def fromInteriorAngle(alpha: AngleDegree): RegularPolygon =
+    360 / alpha.supplement.toRational match
+      case n if n.isWhole => n.toInt
+      case _              => throw new IllegalArgumentException(s"Invalid interior angle: $alpha")
+
   extension (sides: RegularPolygon)
 
     /** @return the underlying number of sides */
